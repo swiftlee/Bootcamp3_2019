bd55b02b44f2c2e84d7199da24e20299e9bfbf5e
Jonathan Conlin <jconlin@ufl.edu>
2019-10-01 07:47:41 -0400

    wiggity wot

--- Bootcamp3/server/config/express.js
+++ Bootcamp3/server/config/express.js
@@ -4,55 +4,56 @@ const path = require('path'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     config = require('./config'),
-    listingsRouter = require('../routes/listings.server.routes'), 
+    listingsRouter = require('../routes/listings.server.routes'),
     getCoordinates = require('../controllers/coordinates.server.controller.js');
 
-module.exports.init = function() {
-  //connect to database
-  mongoose.connect(config.db.uri, { useNewUrlParser: true }).then(() => console.log("Mongo successfully connected.")).catch((err) => console.log(`Mongo error: ${err}`));
+module.exports.init = function () {
+    //connect to database
+    mongoose.connect(config.db.uri, {useNewUrlParser: true}).then(() => console.log("Mongo successfully connected.")).catch((err) => console.log(`Mongo error: ${err}`));
     mongoose.set('useCreateIndex', true);
     mongoose.set('useFindAndModify', false);
 
-  //initialize app
-  const app = express();
+    //initialize app
+    const app = express();
 
-  //enable request logging for development debugging
-  app.use(morgan('dev'));
+    //enable request logging for development debugging
+    app.use(morgan('dev'));
 
-  //body parsing middleware 
-  app.use(bodyParser.json());
+    //body parsing middleware
+    app.use(bodyParser.json());
 
-  /* serve static files - see http://expressjs.com/en/starter/static-files.html */
-  app.use('/', express.static(__dirname + '/../../client'));
+    /* serve static files - see http://expressjs.com/en/starter/static-files.html */
+    app.use('/', express.static(__dirname + '/../../client'));
 
-/* The next three middleware are important to the API that we are bulding */
+    /* The next three middleware are important to the API that we are bulding */
 
-  /* Request Handler for route /api/lisings
-     Update the code to meet the required format - app.use('/api/listings', appropriateMiddlewWare)
-     use the listings router middleware for requests to the api 
-     check the variables list above
-  */
-  app.use('/api/listings', listingsRouter);
-
-
-   /* Request Handler for coordinates
-      This is a server wrapper around Open Cage Data Geocoding API to get latitude + longitude coordinates from address */
-  app.post('/api/coordinates', getCoordinates, function(req, res) {
-    res.send(req.results);
-  });
+    /* Request Handler for route /api/lisings
+       Update the code to meet the required format - app.use('/api/listings', appropriateMiddlewWare)
+       use the listings router middleware for requests to the api
+       check the variables list above
+    */
+    app.use('/api/listings', listingsRouter);
 
 
-  /* Request Handeler for all other routes
-     Sends a response (res) to go to the homepage for all routes not specified */ 
-  app.all('/*', function(req, res) {
-   
-   /*Add YOUR CODE HERE 
-      see https://expressjs.com/en/api.html#res.sendFile
-      see https://nodejs.org/api/path.html
-      The path.resolve() method returns a string and resolves a sequence of paths or path segments into an absolute path.
-      If no path segments are passed, path.resolve() will return the absolute path of the current working directory.
-   */
-   //res.sendFile(path.resolve(...));
-  });
-  return app;
+    /* Request Handler for coordinates
+       This is a server wrapper around Open Cage Data Geocoding API to get latitude + longitude coordinates from address */
+    app.post('/api/coordinates', getCoordinates, function (req, res) {
+        res.send(req.results);
+    });
+
+
+    /* Request Handeler for all other routes
+       Sends a response (res) to go to the homepage for all routes not specified */
+    app.all('/*', function (req, res) {
+
+        /*Add YOUR CODE HERE
+           see https://expressjs.com/en/api.html#res.sendFile
+           see https://nodejs.org/api/path.html
+           The path.resolve() method returns a string and resolves a sequence of paths or path segments into an absolute path.
+           If no path segments are passed, path.resolve() will return the absolute path of the current working directory.
+        */
+        //res.sendFile(path.resolve(...));
+        res.sendFile(path.resolve('client/index.html'));
+    });
+    return app;
 };  
--- Bootcamp3/server/models/listings.server.model.js
+++ Bootcamp3/server/models/listings.server.model.js
@@ -1,24 +1,32 @@
 //You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File
 
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
-    
+    /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
+    code: {type: String, required: true},
+    name: {type: String, required: true},
+    coordinates: {
+        latitude: mongoose.Decimal128,
+        longitude: mongoose.Decimal128
+    },
+    address: String
 });
 
 /* create a 'pre' function that adds the updated_at and created_at if not already there property */
-listingSchema.pre('save', function(next) {
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
+listingSchema.pre('save', function (next) {
+    /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
+    let date = new Date();
+    this.updated_at = date;
 
-  next();
+    if (!this.created_at)
+        this.created_at = date;
+    next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
-var Listing = mongoose.model('Listing', listingSchema);
-
-/* Export the model to make it avaiable to other parts of your Node application */
-module.exports = Listing;
+// consolidated exports for the eyes
+module.exports = Listing = mongoose.model('Listing', listingSchema);
